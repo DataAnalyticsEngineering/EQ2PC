57cdc9611d8379d720e46c292ef4f83dd51c92a8
unknown <mauricio.fernandez.l@gmx.de>
2020-06-13 02:03:16 +0200

    simplified test

--- test_.py
+++ test_.py
@@ -11,18 +11,17 @@ class TestEQ2PC:
     def test_root_gen(self):
         sets1 = e2.Ceq_search([12], [5])
         sets2 = e2.Ceq_search([4, 3], [5])
-        sets3 = e2.Ceq_search([2, 3, 4], [5])
+        #sets3 = e2.Ceq_search([2, 3, 4], [5])
 
         out1 = e2.plot_S(sets1[0][0], show=False)
         out2 = e2.plot_S(sets2[0][0], show=False)
-        out3 = e2.plot_S(sets3[0][0], show=False)
+        #out3 = e2.plot_S(sets3[0][0], show=False)
 
         S1 = sets2[0][0]
         C1s = e2.rCs(S1)
         out4 = e2.plot_C(C1s[0], show=False)
 
-        assert len(sets1) > 0 and len(sets2) > 0 and len(
-            sets3) > 0 and out1 == 0 and out2 == 0 and out3 == 0 and out4 == 0
+        assert len(sets1) > 0 and len(sets2) > 0 and out1 == 0 and out2 == 0 and out3 == 0 and out4 == 0
 
     def test_child_gen(self):
         sets = e2.Ceq_search([4, 3], [5])
